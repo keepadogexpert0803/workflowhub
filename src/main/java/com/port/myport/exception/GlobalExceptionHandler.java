15ba3d06085b779614e2e38f5008fc282787129f
NolanYoon <kiler0803@gmail.com>
2026-07-24 03:46:43 +0900

    예외 응답 공통 처리 추가

---
+++ src/main/java/com/port/myport/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.port.myport.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+}
